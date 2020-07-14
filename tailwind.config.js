413f1d585683bcd7eb04f38ef55387d9d9d11d94
Igor Silveira <igor.silveira@edirectinsure.com>
2020-07-14 19:56:41 +0100

    Added tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'regal-blue': {
+          default: '#243c5a',
+        },
+      },
+    },
+    boxShadow: {
+      default: '0 1px 3px 0 rgba(0, 0, 0, .1), 0 1px 2px 0 rgba(0, 0, 0, .06)',
+      md: '0 4px 25px -1px rgba(0, 0, 0, .1), 0 2px 4px -1px rgba(0, 0, 0, .06)',
+      lg: '0 8px 30px -3px rgba(0, 0, 0, .1), 0 4px 6px -2px rgba(0, 0, 0, .05)',
+      xl: '0 20px 30px -5px rgba(0, 0, 0, .1), 0 10px 10px -5px rgba(0, 0, 0, .04)',
+      '2xl': '0 30px 50px -12px rgba(0, 0, 0, .25)',
+      '3xl': '0 35px 60px -15px rgba(0, 0, 0, .3)',
+      inner: 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)',
+      outline: '0 0 0 3px rgba(66, 153, 225, 0.5)',
+      focus: '0 0 0 3px rgba(66, 153, 225, 0.5)',
+      none: 'none',
+    },
+    borderRadius: {
+      sm: '0.125rem',
+      default: '0.25rem',
+      md: '0.375rem',
+      lg: '0.5rem',
+      full: '9999px',
+      '2lg': '20px',
+    },
+  },
+  variants: {},
+  plugins: [],
+  purge: [
+    './pages/**/*.tsx',
+    './components/**/*.tsx',
+  ],
+};
