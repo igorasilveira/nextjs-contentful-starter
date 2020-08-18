fe6f1e49548cf084b77bf0dde5f6bed0890fcf71
Igor Silveira <igorasilveira@gmail.com>
2020-08-18 12:55:01 +0100

    Individual post page meta

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -15,20 +15,6 @@ export default function Layout({
     <div>
       <Head>
         <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="description"
-          content="Future site of everything web dev"
-        />
-        {/* <meta
-          property="og:image"
-          content={`https://og-image.now.sh/${encodeURI(
-            siteTitle,
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets
-          .vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        /> */}
-        <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
-        <script src="/js/prism.js" async />
       </Head>
       <NavBar />
       <main className="mx-0 mb-16 md:mb-0 md:ml-16 py-8 md:py-12 px-4 md:px-16 xl:px-48 text-rich-black">
