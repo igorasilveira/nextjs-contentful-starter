acc8c185abef28da7c500653e78f43be589063da
Igor Silveira <igorasilveira@gmail.com>
2020-08-18 22:47:21 +0100

    Added fallout for image context

--- components/card/card.tsx
+++ components/card/card.tsx
@@ -11,7 +11,7 @@ export default function Card({
   return (
     <Link href="/posts/[id]" as={`/posts/${post.slug}`}>
       <div className="w-full rounded-2lg overflow-hidden shadow-lg cursor-pointer hover:shadow-2xl transition-shadow duration-300">
-        <img className="w-full h-40 md:h-56 object-cover rounded-r-2lg" src={post.heroImage.url} alt={`${post.title}`} />
+        <img className="w-full h-40 md:h-56 object-cover rounded-r-2lg" src={post.heroImage.url} alt={`${post.heroImage.context.custom.alt || post.title}`} />
         <div className="px-4 pt-4 pb-2">
           <div className="font-semibold text-xl md:text-2xl mb-2 leading-tight">{post.title}</div>
           <div className="font-normal text-sm md:text-base mb-2 text-gray-600 italic"><Date dateString={post.publishDate} /></div>
