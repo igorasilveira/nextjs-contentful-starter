2accc9414e89518f2e0775cdc48d7e98dabcf9c9
Igor Silveira <igorasilveira@gmail.com>
2020-08-18 12:50:56 +0100

    Removed default og:image

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -19,15 +19,15 @@ export default function Layout({
           name="description"
           content="Future site of everything web dev"
         />
-        <meta
+        {/* <meta
           property="og:image"
           content={`https://og-image.now.sh/${encodeURI(
             siteTitle,
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets
+          .vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        /> */}
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
-        <script src="https://kit.fontawesome.com/a8c1cca14c.js" crossOrigin="anonymous" />
         <script src="/js/prism.js" async />
       </Head>
       <NavBar />
